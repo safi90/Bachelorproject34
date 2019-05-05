98e6f1f1df22142bc23c075bba1c0518452c6c5a
safi90 <49609849+safi90@users.noreply.github.com>
2019-05-05 18:45:07 +0200

    Delete SimulationAllCells.R

--- SimulationAllCells.R
+++
@@ -1,8 +0,0 @@
-# Total number of cells
-TotalCells <- nrow(Design)
-for (i in 1:TotalCells){
-  Row <- i
-  MyResult <- MySimulationCell(Design = Design, RowOfDesign = Row, K = 1000 )
-  # Write output of one cell of the design
-  save(MyResult, file =file.path("results",paste0("MyResult", "Row", Row,".Rdata" , sep ="")))
-}
